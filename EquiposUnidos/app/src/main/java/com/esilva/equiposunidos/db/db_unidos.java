5a33038c0b770430cbcd2e50c16dc30ddbfb3fb2
Elkyn <elksil88@gmail.com>
2023-10-18 23:47:05 -0500

    subida de proyecto

---
+++ EquiposUnidos/app/src/main/java/com/esilva/equiposunidos/db/db_unidos.java
@@ -0,0 +1,45 @@
+package com.esilva.equiposunidos.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class db_unidos extends SQLiteOpenHelper {
+    private static final String NAME_DB = "db_unidos";
+    private static final int VERSION = 1;
+
+    //  TABLA  USUARIO
+    public static final String TABLE_USUARIO = "tb_usuario";
+
+    public static final String BASE_CEDULA=     "cedula";
+    public static final String BASE_NOMBRE =    "usuario";
+    public static final String BASE_HUELLA1=     "pulgar";
+    public static final String BASE_HUELLA2=     "indice";
+    public static final String BASE_HUELLA3=     "medio";
+    public static final String BASE_PERFIL=     "perfil";
+    public static final String BASE_CARGO=      "cargo";
+
+
+    public db_unidos(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, NAME_DB, factory, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase Database) {
+        String base = "CREATE TABLE "+ TABLE_USUARIO+"(" +
+                BASE_CEDULA     +" INTEGER PRIMARY KEY, " +
+                BASE_NOMBRE     +" TEXT, " +
+                BASE_PERFIL     +" INTEGER, " +
+                BASE_CARGO      +" TEXT, " +
+                BASE_HUELLA1    +" BLOB, " +
+                BASE_HUELLA2    +" BLOB, " +
+                BASE_HUELLA3    +" BLOB)";
+        Database.execSQL(base);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ EquiposUnidos/app/src/main/java/com/esilva/equiposunidos/util/Constantes.java
@@ -0,0 +1,5 @@
+package com.esilva.equiposunidos.util;
+
+public class Constantes {
+    public static final int version = 2;
+}
